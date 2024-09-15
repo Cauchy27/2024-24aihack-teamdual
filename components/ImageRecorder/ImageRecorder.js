1decac8c61521409aa20a2617f0bdec1718473f5
Kohei Matsukawa <k.matsukawa@dual.tokyo>
2024-09-15 00:28:25 +0000

    テストデータと、細かいデータの追加

--- components/ImageRecorder/ImageRecorder.js
+++ components/ImageRecorder/ImageRecorder.js
@@ -10,15 +10,24 @@ import Typography from "@mui/material";
 
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 import StopCircleIcon from '@mui/icons-material/StopCircle';
+import { Image } from "@mui/icons-material";
 
 import Marquee from "react-fast-marquee";
+
+import SyojikiIcon from "../../public/assets/syojiki-icon.png"
 
 const supabase = createClient(
   "https://hoymimtuzezshkuqcejv.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhveW1pbXR1emV6c2hrdXFjZWp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzMDgwMTksImV4cCI6MjA0MTg4NDAxOX0.yX8rrQx8QHA009vOy8JsIoFIj4eyoL2H7j5PVOxj8_Y"
 );
 
-const testResJson = [`{"audioData": "/outAudio/2024-09-15_00-22-06.wav", "kokoronokoe": "こんなに頑張ったのに、誰も理解してくれないのか。協力した意味がない。もどかしい気持ちが募る。もうどうでもよくなる前に、決意を新たにしなきゃ。"}`,`{"audioData": "/outAudio/2024-09-15_01-52-54.wav", "kokoronokoe": "この瞬間、僕の努力が報われている。皆の反応もいい感じだ。ここまで来たかいがあった。一体、どれだけの時間を費やしてきたんだろう。これが新しい始まりになるといいな。"}`,`{"audioData": "/outAudio/2024-09-15_07-42-30.wav", "kokoronokoe": "こんな場で落ち着いていられるだろうか。周囲は和やかだけど、ふとした瞬間に不安がよぎる。どうにか努めても、内心はイライラしている。もっとスムーズにできたはずなのに、期待に応えられるのか。"}`];
+const testResJson = [
+  `{"audioData": "/outAudio/2024-09-15_00-22-06.wav", "kokoronokoe": "こんなに頑張ったのに、誰も理解してくれないのか。協力した意味がない。もどかしい気持ちが募る。もうどうでもよくなる前に、決意を新たにしなきゃ。"}`,
+  `{"audioData": "/outAudio/2024-09-15_01-52-54.wav", "kokoronokoe": "この瞬間、僕の努力が報われている。皆の反応もいい感じだ。ここまで来たかいがあった。一体、どれだけの時間を費やしてきたんだろう。これが新しい始まりになるといいな。"}`,
+  `{"audioData": "/outAudio/2024-09-15_07-42-30.wav", "kokoronokoe": "こんな場で落ち着いていられるだろうか。周囲は和やかだけど、ふとした瞬間に不安がよぎる。どうにか努めても、内心はイライラしている。もっとスムーズにできたはずなのに、期待に応えられるのか。"}`,
+  `{"audioData": "/outAudio/2024-09-15_09-22-45.wav", "kokoronokoe": "みんなが協力して作り上げたんだ。この瞬間を迎えられて、本当に嬉しい。一緒に頑張ってきた成果を見てほしい。"}`,
+  `{"audioData": "/outAudio/2024-09-15_09-23-31.wav", "kokoronokoe": "緊張する必要はない、全力で伝えるだけだ。皆が高い知識を持っているなら、何も心配はいらない。楽しんでもらえるように、リラックスして進もう。"}`
+];
 
 const testMode = true;
 // const testMode = false;
@@ -36,6 +45,7 @@ export const ImageRecorder = () => {
   const [imageSrc, setImageSrc] = useState(null);
   const [intervalId, setIntervalId] = useState(0);
   const [imageFlag, setImageFlag] = useState(false);
+  const [textSpeed, setTextSpeed] = useState(250);
 
   let firstLoadFlag = false;
 
@@ -95,7 +105,7 @@ export const ImageRecorder = () => {
       test = setInterval(() => {
         console.log("capture!!!",isCaptureEnable)
         capture();
-      },20000);
+      },30000);
 
       setIntervalId(test)
     }
@@ -105,6 +115,7 @@ export const ImageRecorder = () => {
     if(!firstLoadFlag){
       addInterval();
     }
+    console.log(SyojikiIcon);
   },[imageFlag]);
 
   useEffect(()=>{
@@ -135,15 +146,23 @@ export const ImageRecorder = () => {
 
       let kokoronokoeData
       const testNum = Math.random();
-      if(testNum <0.3){
+      if(testNum <0.2){
         kokoronokoeData = JSON.parse(testResJson[0]);
       }
-      else if(testNum <0.6 ){
+      else if(testNum <0.4 ){
         kokoronokoeData = JSON.parse(testResJson[1]);
       }
-      else{
+      else if(testNum <0.6 ){
         kokoronokoeData = JSON.parse(testResJson[2]);
       }
+      else if(testNum <0.8 ){
+        kokoronokoeData = JSON.parse(testResJson[3]);
+      }
+      else{
+        kokoronokoeData = JSON.parse(testResJson[4]);
+      }
+
+      setTextSpeed(kokoronokoeData.kokoronokoe.length * 3.3);
       
       console.log(kokoronokoeData.audioData);
       setKokoronokoeText(kokoronokoeData.kokoronokoe);
@@ -175,6 +194,7 @@ export const ImageRecorder = () => {
         kokoronokoeData = JSON.parse(res_data);
 
         console.log(kokoronokoeData.audioData);
+        setTextSpeed(kokoronokoeData.kokoronokoe.length * 3.3);
         setKokoronokoeText(kokoronokoeData.kokoronokoe);
 
         // 音声のurl取得・urlの形式を後で確認
@@ -193,62 +213,91 @@ export const ImageRecorder = () => {
   // 音声の再生
   // window.AudioContext = window.AudioContext || window.webkitAudioContext;
   let ctx = new AudioContext();
+  let ctxp = new AudioContext();
   let sampleSource
+  let professionalSource
 
    // 音源を取得しAudioBuffer形式に変換して返す関数
- async function setupSample1(soundUrl) {
-  console.log(soundUrl);
-  const response = await fetch(soundUrl);
+  async function setupSample1(soundUrl) {
+    console.log(soundUrl);
+    const response = await fetch(soundUrl);
 
-  console.log(response);
+    console.log(response);
 
-  ctx = new AudioContext();
+    ctx = new AudioContext();
 
-  const arrayBuffer = await response.arrayBuffer();
-  // Web Audio APIで使える形式に変換
-  const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
-  return audioBuffer;
-}
+    const arrayBuffer = await response.arrayBuffer();
+    // Web Audio APIで使える形式に変換
+    const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
+    return audioBuffer;
+  }
+  async function setupProfessional(soundUrl) {
+    console.log(soundUrl);
+    const response = await fetch(soundUrl);
 
-// AudioBufferをctxに接続し再生する関数
-function playSample(ctx, audioBuffer) {
-  sampleSource = ctx.createBufferSource();
-  // 変換されたバッファーを音源として設定
-  sampleSource.buffer = audioBuffer;
-  // 出力につなげる
-  sampleSource.connect(ctx.destination);
-  sampleSource.start();
+    console.log(response);
 
-  sampleSource.onended = function () {
-    console.log("再生終了！") ;
-    setIsSoundPlay(false);
-    setKokoronokoeText("");
-  };
-}
+    ctxp = new AudioContext();
 
-const soundPlay = async(soundUrl) =>{
+    const arrayBuffer = await response.arrayBuffer();
+    // Web Audio APIで使える形式に変換
+    const audioBuffer = await ctxp.decodeAudioData(arrayBuffer);
+    return audioBuffer;
+  }
 
-  console.log(isSoundPlay,soundUrl);
+  // AudioBufferをctxに接続し再生する関数
+  function playSample(ctx, audioBuffer) {
+    sampleSource = ctx.createBufferSource();
+    // 変換されたバッファーを音源として設定
+    sampleSource.buffer = audioBuffer;
+    // 出力につなげる
+    sampleSource.connect(ctx.destination);
+    sampleSource.start();
 
-  if (isSoundPlay) return;
+    sampleSource.onended = function () {
+      console.log("再生終了！") ;
+      setTimeout(()=>{setIsSoundPlay(false);},1000)
+      setKokoronokoeText("");
+    };
+  }
+  function playProfessional(ctx, audioBuffer) {
+    professionalSource = ctx.createBufferSource();
+    // 変換されたバッファーを音源として設定
+    professionalSource.buffer = audioBuffer;
+    // 出力につなげる
+    professionalSource.connect(ctx.destination);
+    professionalSource.start();
+  }
 
-  setIsSoundPlay(true);
-  const sample = await setupSample1(soundUrl);
-  playSample(ctx, sample);
-}
+  const soundPlay = async(soundUrl) =>{
 
-const getSoundTest =async() => {
-  const testUrl = "https://hoymimtuzezshkuqcejv.supabase.co/storage/v1/object/public/media/outAudio/2024-09-15_00-22-06.wav";
+    console.log(isSoundPlay,soundUrl);
 
-  const response = await fetch(testUrl);
+    if (isSoundPlay) return;
 
-  console.log(response);
+    setIsSoundPlay(true);
 
-  soundPlay(testUrl);
+    const professional = await setupProfessional("/assets/professional.mp3");
+    playProfessional(ctxp, professional);
 
-  return "test";
+    const sample = await setupSample1(soundUrl);
+    playSample(ctx, sample);
+    // setTimeout(()=>{
+    // },1000)
+  }
 
-}
+  const getSoundTest =async() => {
+    const testUrl = "https://hoymimtuzezshkuqcejv.supabase.co/storage/v1/object/public/media/outAudio/2024-09-15_00-22-06.wav";
+
+    const response = await fetch(testUrl);
+
+    console.log(response);
+
+    soundPlay(testUrl);
+
+    return "test";
+
+  }
 
   return (
     <>
@@ -267,8 +316,9 @@ const getSoundTest =async() => {
             {
               isSoundPlay &&
               <Marquee
-                speed={250}
-                // loop="1"
+                speed={textSpeed}
+                loop={1}
+                delay={1}
               >
                 <p className="text-7xl text-white font-bold bg-slate-500 opacity-70">{kokoronokoeText}</p>
               </Marquee>
@@ -288,19 +338,30 @@ const getSoundTest =async() => {
         {
           !imageFlag &&
           <IconButton 
-            size="larges" 
+            size="large" 
+            sx={{
+              width:"160px",
+              height:"160px"
+            }}
             onClick={()=>{
               setCaptureEnable(true);
               setImageFlag(true);
             }}
           >
-            <RadioButtonCheckedIcon color="error"/>
+            {/* <RadioButtonCheckedIcon color="error"/> */}
+            <img
+              src={"/assets/syojiki-icon.png"}
+            />
           </IconButton>
         }
         {
           imageFlag &&
           <IconButton 
-            size="larges" 
+            size="large" 
+            sx={{
+              width:"160px",
+              height:"160px"
+            }}
             onClick={()=>{
                   setImageFlag(false);
                   clearInterval(intervalId);
@@ -308,7 +369,10 @@ const getSoundTest =async() => {
                   setCaptureEnable(false);
             }}
           >
-            <StopCircleIcon color=""/>
+            {/* <StopCircleIcon color=""/> */}
+            <img
+              src={"/assets/syojiki-stop-icon.png"}
+            />
           </IconButton>
         }
 
